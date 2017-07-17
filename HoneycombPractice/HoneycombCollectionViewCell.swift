8384b694a7688b49eb9b9f996627fff6f55ed26f
Hanyu <rollr76518@gmail.com>
2017-07-17 11:03:55 +0800

    - 新增 gitignore - 移除多餘的圖示 - 完成主要的蜂巢設計

--- HoneycombPractice/HoneycombCollectionViewCell.swift
+++ HoneycombPractice/HoneycombCollectionViewCell.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class HoneycombCollectionViewCell: UICollectionViewCell {
 
-	@IBOutlet weak var imageView: UIImageView!
 	@IBOutlet weak var label: UILabel!
-	
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+	var path:UIBezierPath!
 
+    override func awakeFromNib()
+	{
+        super.awakeFromNib()
+		
+		backgroundView = UIImageView.init(image: #imageLiteral(resourceName: "SB_Music_Balloon_Cell_nested"))
+		selectedBackgroundView = UIImageView.init(image: #imageLiteral(resourceName: "SB_REC_Balloon_Cell_nested"))
+    }
+	
+	override func draw(_ rect: CGRect) {
+		super.draw(rect)
+		
+		path = UIBezierPath.init(rect: CGRect.init(x: frame.size.width/2, y: 0, width: 0, height: 0))
+		path.addLine(to: CGPoint.init(x: 0, y: frame.size.height/2))
+		path.addLine(to: CGPoint.init(x: frame.size.width/2, y: frame.size.height))
+		path.addLine(to: CGPoint.init(x: frame.size.width, y: frame.size.height/2))
+		path.close()
+		path.lineWidth = 1.0
+		UIColor.green.setStroke()//展示用，請自行拿掉
+		path.stroke()
+	}
+	
+	override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+		if path.contains(point) {
+			return true
+		}
+		else {
+			return false
+		}
+	}
 }
--- HoneycombPractice/ViewController.swift
+++ HoneycombPractice/ViewController.swift
@@ -16,13 +16,15 @@ class ViewController: UIViewController {
 	let cellWidth = (UIScreen.main.bounds.width - 3)/4
 	let cellHeight = (UIScreen.main.bounds.width - 3)/4 * 240/156
 	
-	override func viewDidLoad() {
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
 		
 		collectionView.register(UINib.init(nibName: cellID, bundle: nil), forCellWithReuseIdentifier: cellID)
 	}
 
-	override func didReceiveMemoryWarning() {
+	override func didReceiveMemoryWarning()
+	{
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
@@ -30,8 +32,9 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
 {
-	func numberOfSections(in collectionView: UICollectionView) -> Int {
-		return 100
+	func numberOfSections(in collectionView: UICollectionView) -> Int
+	{
+		return 30
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
@@ -48,28 +51,31 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, 
 	{
 		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! HoneycombCollectionViewCell
 		
-		cell.imageView.image = #imageLiteral(resourceName: "SB_SFX_Balloon_Cell_nested")
 		cell.label.text = String(describing: indexPath)
 		
 		return cell
 	}
 	
-	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+	{
 		let width = cellWidth
 		let height = cellHeight
 		
 		return CGSize.init(width: width, height: height)
 	}
 
-	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat
+	{
 		return 1
 	}
 	
-	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat
+	{
 		return -(cellHeight/2)
 	}
 	
-	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets
+	{
 		if section%2 == 0 {
 			let insets = UIEdgeInsets.init(top: 0, left: 0, bottom: -(cellHeight/2), right: 0)
 			return insets
@@ -79,5 +85,4 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, 
 			return insets
 		}
 	}
-	
 }
